037e0fe82aed1fe76f889b8674167999f7914204
Ana Riccetti <anariccetti@gmail.com>
2018-11-14 11:29:48 -0200

    migrations change

--- db/migrate/20181113154638_create_authorships.rb
+++ db/migrate/20181113154638_create_authorships.rb
@@ -1,6 +1,6 @@
-class CreateAutorships < ActiveRecord::Migration[5.2]
+class CreateAuthorships < ActiveRecord::Migration[5.2]
   def change
-    create_table :autorships do |t|
+    create_table :authorships do |t|
       t.references :project, foreign_key: true
       t.references :councillor, foreign_key: true
 
