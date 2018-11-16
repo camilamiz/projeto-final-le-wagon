cc3795b6adf3dfb71c13f49b4ab84a81829240e0
Lucas Otsuka <otsuka.lucas@gmail.com>
2018-11-16 14:15:50 -0200

    @parties_projects funcionando. Hash com freequencia de projetos por partido desde 2017

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,22 @@
 class ProjectsController < ApplicationController
   def index
     @projects = Project.all
+    @parties_projects = count_projects
   end
 
   def show
     @project = Project.find(params[:id])
+  end
+
+  private
+
+  def count_projects
+    hash = {}
+    Project.where("ano > ?", 2016).each do |project|
+      proj_parties = project.authorships.map { |p| Councillor.find(p[:councillor_id]).party }.uniq
+      proj_parties.each do |partido|
+        if hash.key?(partido) ? hash[partido] += 1 : hash[partido] = 1
+      end
+    end
   end
 end
