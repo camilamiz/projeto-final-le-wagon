2ff53c38293b81d87d97458fe63de46afc1bbc7e
Lucas Otsuka <otsuka.lucas@gmail.com>
2018-11-21 15:41:22 -0200

    Voting table with result and chave now. Also now we get symbolic votings

--- db/seeds.rb
+++ db/seeds.rb
@@ -116,64 +116,129 @@ years_array = (2012..2018).to_a
 ##############################################################
 # # Puxando votaçoes
 
-# # Voting.delete_all
-# # ActiveRecord::Base.connection.reset_pk_sequence!('votings')
+Voting.delete_all
+ActiveRecord::Base.connection.reset_pk_sequence!('votings')
 
-# puts "Parsing votings..."
+puts "Parsing votings..."
 
-# years_array.each do |year|
-#   puts "...from year #{year}..."
+years_array.each do |year|
+  puts "...from year #{year}..."
 
-#   document  = Nokogiri::XML(open("https://splegispdarmazenamento.blob.core.windows.net/containersip/VOTACOES_#{year}.xml"))
-#   document.xpath('/CMSP/Sessao').each do |sessao|
-#     data =  Date.parse(sessao['Data'])
-#     session_name = sessao['Nome']
-#     sessao.css('Votacao').each do |votacao|
-#       tipo = votacao['TipoVotacao']
-#       ementa = votacao['Ementa']
-#       rodape = votacao['NotasRodape']
-#       materia = votacao['Materia']
+  document  = Nokogiri::XML(open("https://splegispdarmazenamento.blob.core.windows.net/containersip/VOTACOES_#{year}.xml"))
+  document.xpath('/CMSP/Sessao').each do |sessao|
+    data =  Date.parse(sessao['Data'])
+    session_name = sessao['Nome']
+    sessao.css('Votacao').each do |votacao|
+      tipo = votacao['TipoVotacao']
+      ementa = votacao['Ementa']
+      rodape = votacao['NotasRodape']
+      materia = votacao['Materia']
+      chave = votacao['VotacaoID'].to_i
 
-#       array = materia.split(' ')
-#       projeto_index = array.index { |e| e =~ /\d+\/\d+/ }
-#       if projeto_index.nil? == false
-#         tipo_projeto = array[projeto_index - 1]
-#         num_ano = array.select { |i| i[/\d+\/\d+/] }[0].split('/')
-#       else
-#         tipo_projeto = nil
-#         num_ano = ["0", "0"]
-#       end
+      if votacao['Resultado'] = "Aprovado1"
+        resultado = "Aprovado"
+      else
+        resultado = votacao['Resultado']
+      end
 
-#       project = Project.find_by(numero: num_ano[0].to_i, ano: num_ano[1].to_i, tipo: tipo_projeto )
+      # p materia.scan(/[A-Z]+\s\d+\s?\/\d+/).empty?
+      if materia.scan(/[A-Z]+\s\d+\s?\/\d+/).empty? == false
+        array = materia.scan(/[A-Z]+\s\d+\s?\/\d+/).first.split(' ')
+        tipo_projeto = array[0]
+        if array.length == 2
+          num_ano = array[1].scan(/\d+\s?\/\d+/)[0].split('/')
+        elsif array.length == 3
+          num_ano = [array[1], array[2].scan(/\d+/).first]
+        end
+      else
+        tipo_projeto = nil
+        num_ano = ["0", "0"]
+      end
 
-#       if project.nil? == false
-#         project_id = project.id
-#         votacao.css("Vereador").each do |vereador|
-#           councillor_id = Councillor.find_by(chave: vereador['IDParlamentar'].to_i).id
-#           partido = project.authorships.first.councillor.party
+      project = Project.find_by(numero: num_ano[0].to_i, ano: num_ano[1].to_i, tipo: tipo_projeto )
 
-#           vote = vereador['Voto'] == 'Sim'
+      if project.nil? == false
+        project_id = project.id
+        partido = project.authorships.first.councillor.party
+        if tipo == "Nominal"
+          votacao.css("Vereador").each do |vereador|
+            councillor_id = Councillor.find_by(chave: vereador['IDParlamentar'].to_i).id
 
-#           Voting.create!(
-#             vote_date: data,
-#             councillor_id: councillor_id,
-#             project_id: project_id,
-#             vote: vote,
-#             sessao: session_name,
-#             tipo: tipo,
-#             materia: materia,
-#             ementa: ementa,
-#             rodape: rodape,
-#             partido: partido
-#           )
-#         end
-#       end
-#     end
-#   end
-#   puts "Done parsing #{year}"
-# end
+            vote = vereador['Voto'] == 'Sim'
 
-# puts "Done parsing votings!"
+            Voting.create!(
+              vote_date: data,
+              councillor_id: councillor_id,
+              project_id: project_id,
+              vote: vote,
+              sessao: session_name,
+              tipo: tipo,
+              materia: materia,
+              ementa: ementa,
+              rodape: rodape,
+              partido: partido,
+              resultado: resultado,
+              chave: chave
+            )
+          end
+        elsif tipo == "Simbólica"
+          if votacao.css("VotoContrario").empty?
+            # p "simbolica"
+            Voting.create!(
+              vote_date: data,
+              project_id: project_id,
+              sessao: session_name,
+              tipo: tipo,
+              materia: materia,
+              ementa: ementa,
+              rodape: rodape,
+              partido: partido,
+              resultado: resultado,
+              chave: chave
+            )
+          else
+            votacao.css("VotoContrario").each do |vereador|
+              unless vereador["Partido_Vereador"] == "PSDB" || vereador["Partido_Vereador"] == "PSOL" || vereador["Partido_Vereador"] == "PT"
+                counc_name = vereador["Partido_Vereador"].split(" - ")[0].upcase
+
+                # EXCEÇÕES
+                counc_name = "JOSE AMERICO" if counc_name == "José Américo".upcase
+                counc_name = "MARCO AURELIO CUNHA" if counc_name == "Marco Aurélio Cunha".upcase
+                counc_name = "MARIO COVAS NETO" if counc_name == "Mário Covas Neto".upcase
+                counc_name = "AURÉLIO NOMURA" if counc_name == "Aurelio Nomura".upcase
+                counc_name = "JANAÍNA LIMA" if counc_name == "Janaina Lima".upcase
+                counc_name = "ATÍLIO FRANCISCO" if counc_name == "Atilio Francisco".upcase
+
+                p counc_name
+                p data
+                councillor_id = Councillor.find_by(name: counc_name).id
+                p councillor_id
+                vote = false
+                Voting.create!(
+                vote_date: data,
+                councillor_id: councillor_id,
+                project_id: project_id,
+                vote: vote,
+                sessao: session_name,
+                tipo: tipo,
+                materia: materia,
+                ementa: ementa,
+                rodape: rodape,
+                partido: partido,
+                resultado: resultado,
+                chave: chave
+                )
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+  puts "Done parsing #{year}"
+end
+
+puts "Done parsing votings!"
 
 
 # # # Definindo os partidos dos ultimos prefeitos de São Paulo
@@ -201,76 +266,84 @@ years_array = (2012..2018).to_a
 
 # # Puxando Status de cada Projeto de cada Councillor
 
-puts "Parsing Project Status..."
+# puts "Parsing Project Status..."
 
-Project.update_all("status = ''");
+# Project.update_all("status = ''");
 
-Councillor.where.not(party: nil).each do |vereador|
+# # Projetos em Tramitação
 
-  url = "http://splegisws.camara.sp.gov.br/ws/ws2.asmx/ProjetosEmTramitacaoPorPromoventeJSON?codigo=#{vereador.chave}"
-  projetos_serialized = open(url).read
-  projetos = JSON.parse(projetos_serialized)
-  projetos.each do |projeto|
-    if projeto["ano"] >= 2012
-      project = Project.find_by("tipo = ? AND ano = ? AND numero = ?",
-                            projeto["tipo"], projeto["ano"], projeto["numero"])
-      project.status = "Em Tramitação"
-      project.save
-    end
-  end
+# Councillor.where.not(party: nil).each do |vereador|
 
-  url = "http://splegisws.camara.sp.gov.br/ws/ws2.asmx/LeisAprovadasPorPromoventeJSON?codigo=#{vereador.chave}"
-  projetos_serialized = open(url).read
-  projetos = JSON.parse(projetos_serialized)
-  projetos.each do |projeto|
-    if projeto["projeto"]["ano"] >= 2012
-      project = Project.find_by("tipo = ? AND ano = ? AND numero = ?",
-                            projeto["projeto"]["tipo"], projeto["projeto"]["ano"], projeto["projeto"]["numero"])
+#   url = "http://splegisws.camara.sp.gov.br/ws/ws2.asmx/ProjetosEmTramitacaoPorPromoventeJSON?codigo=#{vereador.chave}"
+#   projetos_serialized = open(url).read
+#   projetos = JSON.parse(projetos_serialized)
+#   projetos.each do |projeto|
+#     if projeto["ano"] >= 2012
+#       project = Project.find_by("tipo = ? AND ano = ? AND numero = ?",
+#                             projeto["tipo"], projeto["ano"], projeto["numero"])
+#       project.status = "Em Tramitação"
+#       project.save
+#     end
+#   end
 
-      project.status = "Aprovado"
-      project.save
-    end
-  end
+# # Projetos Aprovados
 
-  url = "http://splegisws.camara.sp.gov.br/ws/ws2.asmx/ProjetosVetadosPorPromoventeJSON?codigo=#{vereador.chave}"
-  projetos_serialized = open(url).read
-  projetos = JSON.parse(projetos_serialized)
-  projetos.each do |projeto|
-    if projeto["ano"] >= 2012
-      project = Project.find_by("tipo = ? AND ano = ? AND numero = ?",
-                            projeto["tipo"], projeto["ano"], projeto["numero"])
+#   url = "http://splegisws.camara.sp.gov.br/ws/ws2.asmx/LeisAprovadasPorPromoventeJSON?codigo=#{vereador.chave}"
+#   projetos_serialized = open(url).read
+#   projetos = JSON.parse(projetos_serialized)
+#   projetos.each do |projeto|
+#     if projeto["projeto"]["ano"] >= 2012
+#       project = Project.find_by("tipo = ? AND ano = ? AND numero = ?",
+#                             projeto["projeto"]["tipo"], projeto["projeto"]["ano"], projeto["projeto"]["numero"])
 
-      project.status = "Vetado"
-      project.save
-    end
-  end
-end
+#       project.status = "Aprovado"
+#       project.save
+#     end
+#   end
 
-counter_new_status = 0
-counter_update_status = 0
-array = []
+# # Projetos Vetados
 
-years_array.each do |year|
-  url = "http://splegisws.camara.sp.gov.br/ws/ws2.asmx/ProjetosEncerradosJSON?ano=#{year}"
-  projetos_serialized = open(url).read
-  projetos = JSON.parse(projetos_serialized)
-  projetos.each do |projeto|
-    project = Project.find_by("tipo = ? AND ano = ? AND numero = ?",
-                          projeto["tipo"], projeto["ano"], projeto["numero"])
+#   url = "http://splegisws.camara.sp.gov.br/ws/ws2.asmx/ProjetosVetadosPorPromoventeJSON?codigo=#{vereador.chave}"
+#   projetos_serialized = open(url).read
+#   projetos = JSON.parse(projetos_serialized)
+#   projetos.each do |projeto|
+#     if projeto["ano"] >= 2012
+#       project = Project.find_by("tipo = ? AND ano = ? AND numero = ?",
+#                             projeto["tipo"], projeto["ano"], projeto["numero"])
 
-    case projeto["motivo"]
-    when "TERMINO DE LEGISLATURA (ART. 275 REG. INT.)"
-      project.status = "Encerrado por Término de Legislatura"
-    when "Encerrado-ILEGALIDADE (ART. 79 REG. INT.)"
-      project.status = "Encerrado por Ilegalidade"
-    when "Encerrado-RETIRADO PELO AUTOR"
-      project.status = "Retirado pelo autor"
-    when "Encerrado-PROMULGADO"
-      project.status = "Promulgado"
-    end
-    project.save
-  end
-end
+#       project.status = "Vetado"
+#       project.save
+#     end
+#   end
+# end
+
+# counter_new_status = 0
+# counter_update_status = 0
+# array = []
+
+# # Projetos Encerrados
+
+# years_array.each do |year|
+#   url = "http://splegisws.camara.sp.gov.br/ws/ws2.asmx/ProjetosEncerradosJSON?ano=#{year}"
+#   projetos_serialized = open(url).read
+#   projetos = JSON.parse(projetos_serialized)
+#   projetos.each do |projeto|
+#     project = Project.find_by("tipo = ? AND ano = ? AND numero = ?",
+#                           projeto["tipo"], projeto["ano"], projeto["numero"])
+
+#     case projeto["motivo"]
+#     when "TERMINO DE LEGISLATURA (ART. 275 REG. INT.)"
+#       project.status = "Encerrado por Término de Legislatura"
+#     when "Encerrado-ILEGALIDADE (ART. 79 REG. INT.)"
+#       project.status = "Encerrado por Ilegalidade"
+#     when "Encerrado-RETIRADO PELO AUTOR"
+#       project.status = "Retirado pelo autor"
+#     when "Encerrado-PROMULGADO"
+#       project.status = "Promulgado"
+#     end
+#     project.save
+#   end
+# end
 
 ##############################################################
 
