b563684e4606beb6c1213c2ec3b67d97791a20fd
Ana Riccetti <anariccetti@gmail.com>
2018-11-21 18:40:06 -0200

    councillors show

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,66 @@ require 'nokogiri'
 # # Definindo o range de anos do seed
 years_array = (2012..2018).to_a
 
+# # Incluindo as fotos dos vereadores
+
+photos = {
+'2156' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2017/01/Retrato-Caio-Perfil-Facebook_ADP-1.jpg',
+'776' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/Foto_Eliseu_Gabriel_portal.jpg',
+'1326' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2015/03/quito-formiga-2017-1.jpg',
+'160' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2017/01/eduardosuplicy2017.jpg',
+'1999' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/toninho-vespoli.jpg',
+'2147' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2017/01/isac_felix_2017.jpg',
+'2174' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2017/05/amauri-direita_193_155.png',
+'2151' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2016/12/ZT-6_corte_192.png',
+'1930' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/ota.jpg',
+'2160' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2017/01/joao_jorge_2017.jpg',
+'187' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/gilson-barreto.jpg',
+'224' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/aurelio-nomura.jpg',
+'1707' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/Vereador_DavidSoares_2017v2.jpg',
+'220' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/toninho-paiva.jpg',
+'1305' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2015/03/Police_perfil.jpg',
+'2145' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2017/01/fernando_holi_2017.jpg',
+'1947' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/reis.jpg',
+'1319' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2017/01/soninha_193_155-1.jpg',
+'1636' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2017/01/DRMilton_Ferreira_2017.jpg',
+'1316' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/Noemi_jum_2017.jpg',
+'1634' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/Juliana-Cardoso.jpg',
+'1647' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/edir_sales_2017.jpg',
+'2002' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/conte-lopes.jpg',
+'756' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2017/01/celso_jatene2017.jpg',
+'1633' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/Vereador_SouzaSantos_2017v2.jpg',
+'2152' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2017/01/rinaldi.jpg',
+'676' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/atilio-francisco.jpg',
+'2158' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2017/02/Vereador_ReginaldoTripoli_2017v2.jpg',
+'2001' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/jair-tatto.jpg',
+'2000' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2017/01/alessandro_guedes_2017.jpg',
+'2142' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2016/12/Adriana-Ramalho-site-CMSP.jpg',
+'796' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/natalini.jpg',
+'268' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/dalton-silvano.jpg',
+'2144' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2017/01/fabio_riva_2017.jpg',
+'2159' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2017/01/rute_costa_193_155.jpg',
+'155' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/arselino-tatto.jpg',
+'896' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2017/01/professor_claudinho.jpg',
+'1300' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/claudinho-de-souza.jpg',
+'2150' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2017/01/AndreSantos_jun_2017.jpg',
+'44' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/mario_covas-site.jpg',
+'2154' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2017/01/camilo_cristo_2017.jpg',
+'267' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/frange.jpg',
+'1311' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/donato.jpg',
+'1635' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/sandra_tadeu_193_155.jpg',
+'1990' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/ricardo-nunes.jpg',
+'2155' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2017/01/janaina_lima_2017.jpg',
+'1349' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/senival-moura.jpg',
+'1487' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2016/02/rteixeira_lan16-1.jpg',
+'1998' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/12/Patricia-Bezerra.jpg',
+'273' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/Milton_Leite_2017maio.jpg',
+'1447' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/14560192_1134347556634715_1888787081855639171_o-1-1.jpg',
+'1989' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2014/10/george-hato.jpg',
+'1296' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2017/02/Vereador_AdilsonAmadeu_2017v2.jpg',
+'2148' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2017/01/samia_bomfim_2017.jpg',
+'2153' => 'http://www.saopaulo.sp.leg.br/wp-content/uploads/2017/01/Rodrigo_Goulart_2017.jpg'
+}
+
 ##############################################################
 # # Puxando Vereadores (Somente com o nome e chave. Ainda falta definir os partidos)
 
@@ -23,8 +83,11 @@ years_array = (2012..2018).to_a
 #   councillor = Councillor.create!(name: vereador["nome"], chave: vereador["chave"])
 # end
 
-
-# puts "Done with Councillors!"
+Councillor.all.each do |councillor|
+  councillor.photo = photos[councillor[:chave].to_s]
+  councillor.save
+end
+puts "Done with Councillors!"
 
 # ##############################################################
 # # Puxando Attendance
@@ -116,126 +179,126 @@ years_array = (2012..2018).to_a
 ##############################################################
 # # Puxando votaçoes
 
-Voting.delete_all
-ActiveRecord::Base.connection.reset_pk_sequence!('votings')
+# Voting.delete_all
+# ActiveRecord::Base.connection.reset_pk_sequence!('votings')
 
-puts "Parsing votings..."
+# puts "Parsing votings..."
 
-years_array.each do |year|
-  puts "...from year #{year}..."
+# years_array.each do |year|
+#   puts "...from year #{year}..."
 
-  document  = Nokogiri::XML(open("https://splegispdarmazenamento.blob.core.windows.net/containersip/VOTACOES_#{year}.xml"))
-  document.xpath('/CMSP/Sessao').each do |sessao|
-    data =  Date.parse(sessao['Data'])
-    session_name = sessao['Nome']
-    sessao.css('Votacao').each do |votacao|
-      tipo = votacao['TipoVotacao']
-      ementa = votacao['Ementa']
-      rodape = votacao['NotasRodape']
-      materia = votacao['Materia']
-      chave = votacao['VotacaoID'].to_i
+#   document  = Nokogiri::XML(open("https://splegispdarmazenamento.blob.core.windows.net/containersip/VOTACOES_#{year}.xml"))
+#   document.xpath('/CMSP/Sessao').each do |sessao|
+#     data =  Date.parse(sessao['Data'])
+#     session_name = sessao['Nome']
+#     sessao.css('Votacao').each do |votacao|
+#       tipo = votacao['TipoVotacao']
+#       ementa = votacao['Ementa']
+#       rodape = votacao['NotasRodape']
+#       materia = votacao['Materia']
+#       chave = votacao['VotacaoID'].to_i
 
-      if votacao['Resultado'] = "Aprovado1"
-        resultado = "Aprovado"
-      else
-        resultado = votacao['Resultado']
-      end
+#       if votacao['Resultado'] = "Aprovado1"
+#         resultado = "Aprovado"
+#       else
+#         resultado = votacao['Resultado']
+#       end
 
-      # p materia.scan(/[A-Z]+\s\d+\s?\/\d+/).empty?
-      if materia.scan(/[A-Z]+\s\d+\s?\/\d+/).empty? == false
-        array = materia.scan(/[A-Z]+\s\d+\s?\/\d+/).first.split(' ')
-        tipo_projeto = array[0]
-        if array.length == 2
-          num_ano = array[1].scan(/\d+\s?\/\d+/)[0].split('/')
-        elsif array.length == 3
-          num_ano = [array[1], array[2].scan(/\d+/).first]
-        end
-      else
-        tipo_projeto = nil
-        num_ano = ["0", "0"]
-      end
+#       # p materia.scan(/[A-Z]+\s\d+\s?\/\d+/).empty?
+#       if materia.scan(/[A-Z]+\s\d+\s?\/\d+/).empty? == false
+#         array = materia.scan(/[A-Z]+\s\d+\s?\/\d+/).first.split(' ')
+#         tipo_projeto = array[0]
+#         if array.length == 2
+#           num_ano = array[1].scan(/\d+\s?\/\d+/)[0].split('/')
+#         elsif array.length == 3
+#           num_ano = [array[1], array[2].scan(/\d+/).first]
+#         end
+#       else
+#         tipo_projeto = nil
+#         num_ano = ["0", "0"]
+#       end
 
-      project = Project.find_by(numero: num_ano[0].to_i, ano: num_ano[1].to_i, tipo: tipo_projeto )
+#       project = Project.find_by(numero: num_ano[0].to_i, ano: num_ano[1].to_i, tipo: tipo_projeto )
 
-      if project.nil? == false
-        project_id = project.id
-        partido = project.authorships.first.councillor.party
-        if tipo == "Nominal"
-          votacao.css("Vereador").each do |vereador|
-            councillor_id = Councillor.find_by(chave: vereador['IDParlamentar'].to_i).id
+#       if project.nil? == false
+#         project_id = project.id
+#         partido = project.authorships.first.councillor.party
+#         if tipo == "Nominal"
+#           votacao.css("Vereador").each do |vereador|
+#             councillor_id = Councillor.find_by(chave: vereador['IDParlamentar'].to_i).id
 
-            vote = vereador['Voto'] == 'Sim'
+#             vote = vereador['Voto'] == 'Sim'
 
-            Voting.create!(
-              vote_date: data,
-              councillor_id: councillor_id,
-              project_id: project_id,
-              vote: vote,
-              sessao: session_name,
-              tipo: tipo,
-              materia: materia,
-              ementa: ementa,
-              rodape: rodape,
-              partido: partido,
-              resultado: resultado,
-              chave: chave
-            )
-          end
-        elsif tipo == "Simbólica"
-          if votacao.css("VotoContrario").empty?
-            # p "simbolica"
-            Voting.create!(
-              vote_date: data,
-              project_id: project_id,
-              sessao: session_name,
-              tipo: tipo,
-              materia: materia,
-              ementa: ementa,
-              rodape: rodape,
-              partido: partido,
-              resultado: resultado,
-              chave: chave
-            )
-          else
-            votacao.css("VotoContrario").each do |vereador|
-              unless vereador["Partido_Vereador"] == "PSDB" || vereador["Partido_Vereador"] == "PSOL" || vereador["Partido_Vereador"] == "PT"
-                counc_name = vereador["Partido_Vereador"].split(" - ")[0].upcase
+#             Voting.create!(
+#               vote_date: data,
+#               councillor_id: councillor_id,
+#               project_id: project_id,
+#               vote: vote,
+#               sessao: session_name,
+#               tipo: tipo,
+#               materia: materia,
+#               ementa: ementa,
+#               rodape: rodape,
+#               partido: partido,
+#               resultado: resultado,
+#               chave: chave
+#             )
+#           end
+#         elsif tipo == "Simbólica"
+#           if votacao.css("VotoContrario").empty?
+#             # p "simbolica"
+#             Voting.create!(
+#               vote_date: data,
+#               project_id: project_id,
+#               sessao: session_name,
+#               tipo: tipo,
+#               materia: materia,
+#               ementa: ementa,
+#               rodape: rodape,
+#               partido: partido,
+#               resultado: resultado,
+#               chave: chave
+#             )
+#           else
+#             votacao.css("VotoContrario").each do |vereador|
+#               unless vereador["Partido_Vereador"] == "PSDB" || vereador["Partido_Vereador"] == "PSOL" || vereador["Partido_Vereador"] == "PT"
+#                 counc_name = vereador["Partido_Vereador"].split(" - ")[0].upcase
 
-                # EXCEÇÕES
-                counc_name = "JOSE AMERICO" if counc_name == "José Américo".upcase
-                counc_name = "MARCO AURELIO CUNHA" if counc_name == "Marco Aurélio Cunha".upcase
-                counc_name = "MARIO COVAS NETO" if counc_name == "Mário Covas Neto".upcase
-                counc_name = "AURÉLIO NOMURA" if counc_name == "Aurelio Nomura".upcase
-                counc_name = "JANAÍNA LIMA" if counc_name == "Janaina Lima".upcase
-                counc_name = "ATÍLIO FRANCISCO" if counc_name == "Atilio Francisco".upcase
+#                 # EXCEÇÕES
+#                 counc_name = "JOSE AMERICO" if counc_name == "José Américo".upcase
+#                 counc_name = "MARCO AURELIO CUNHA" if counc_name == "Marco Aurélio Cunha".upcase
+#                 counc_name = "MARIO COVAS NETO" if counc_name == "Mário Covas Neto".upcase
+#                 counc_name = "AURÉLIO NOMURA" if counc_name == "Aurelio Nomura".upcase
+#                 counc_name = "JANAÍNA LIMA" if counc_name == "Janaina Lima".upcase
+#                 counc_name = "ATÍLIO FRANCISCO" if counc_name == "Atilio Francisco".upcase
 
-                councillor_id = Councillor.find_by(name: counc_name).id
-                vote = false
-                Voting.create!(
-                vote_date: data,
-                councillor_id: councillor_id,
-                project_id: project_id,
-                vote: vote,
-                sessao: session_name,
-                tipo: tipo,
-                materia: materia,
-                ementa: ementa,
-                rodape: rodape,
-                partido: partido,
-                resultado: resultado,
-                chave: chave
-                )
-              end
-            end
-          end
-        end
-      end
-    end
-  end
-  puts "Done parsing #{year}"
-end
+#                 councillor_id = Councillor.find_by(name: counc_name).id
+#                 vote = false
+#                 Voting.create!(
+#                 vote_date: data,
+#                 councillor_id: councillor_id,
+#                 project_id: project_id,
+#                 vote: vote,
+#                 sessao: session_name,
+#                 tipo: tipo,
+#                 materia: materia,
+#                 ementa: ementa,
+#                 rodape: rodape,
+#                 partido: partido,
+#                 resultado: resultado,
+#                 chave: chave
+#                 )
+#               end
+#             end
+#           end
+#         end
+#       end
+#     end
+#   end
+#   puts "Done parsing #{year}"
+# end
 
-puts "Done parsing votings!"
+# puts "Done parsing votings!"
 
 
 # # # Definindo os partidos dos ultimos prefeitos de São Paulo
